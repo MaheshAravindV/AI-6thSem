d40e3a17b855f74ec21066911e01d27536812633
MaheshAravindV <mahesharavind@student.tce.edu>
2022-03-06 14:39:49 +0530

    Added documentation. AI part has to be added

--- GridPuzzle/index.js
+++ GridPuzzle/index.js
@@ -1,23 +1,26 @@
-size = window.prompt("Size");
+size = window.prompt("Size"); //Size should preferably be between 3 and 6 for UI to look good
 
-board = [];
-goal = [];
+board = []; //Board is the current state of the game
+goal = []; //This is the final target state
 
 holerow = size - 1;
-holecol = size - 1;
+holecol = size - 1; //To keep track of where the hole is
+
 for (row = 0; row < size; row++) {
+  //Creating the initial state. (We will shuffle in the end)
   board.push([]);
   goal.push([]);
   for (col = 0; col < size; col++)
-    board[row].push(row * size + col + 1), goal[row].push(board[row][col]);
+    board[row].push(row * size + col + 1), goal[row].push(board[row][col]); //Goal is copied from board as the target is like [[1,2,3],[4,5,6],[7,8,0]]
 }
 
 board[holerow][holecol] = 0;
-goal[holerow][holecol] = 0;
+goal[holerow][holecol] = 0; //Setting the hole
 
-padding = 60 - (size - 3) * 10;
+padding = 60 - (size - 3) * 10; //Padding must be programatically given as smaller cells look better when there are more cells 👇🏻Line 30
 
 async function updateTable(board) {
+  //Function to update UI based on the board state
   var table = document.getElementsByTagName("table")[0];
   table.innerHTML = "";
   board.forEach((row) => {
@@ -25,8 +28,9 @@ async function updateTable(board) {
     row.forEach((cell) => {
       var newCell = document.createElement("td");
       if (cell != 0) newCell.textContent = cell;
+      //If value is 0, means there is a hole there. If not, add text content to the cell
       else newCell.className = "empty-cell";
-      newCell.style.padding = padding + "px";
+      newCell.style.padding = padding + "px"; //This is where Padding is set based on size
       newRow.appendChild(newCell);
     });
     table.appendChild(newRow);
@@ -34,11 +38,13 @@ async function updateTable(board) {
 }
 
 async function shuffle() {
+  //To shuffle the state in the beginning of each run
   options = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"];
-  for (i = 0; i < 100 * (size - 3); i++) {
-    var rand = options[~~(Math.random() * options.length)];
+  for (i = 0; i < 100 * (size - 2); i++) {
+    var rand = options[~~(Math.random() * options.length)]; //Randomly picking a move (Sometimes, the picked move is invalid. In that case, nothing happens)
     window.dispatchEvent(
       new KeyboardEvent("keydown", {
+        //Using our own event listener to shuffle by giving moves in a random order
         key: rand,
       })
     );
@@ -46,7 +52,9 @@ async function shuffle() {
 }
 
 window.addEventListener("keydown", async (e) => {
-  switch (e.key) {
+  switch (
+    e.key //Based on the key pressed, if the move is valid, the new cell must come where the hole used to be and the hole must be moved
+  ) {
     case "ArrowDown":
       if (holerow > 0) {
         board[holerow][holecol] = board[holerow - 1][holecol];
@@ -75,11 +83,11 @@ window.addEventListener("keydown", async (e) => {
         board[holerow][holecol] = 0;
       }
       break;
-    case "r":
+    case "r": //r can be pressed to reload the site
       shuffle();
       break;
   }
   updateTable(board);
 });
 
-window.onload = shuffle;
+window.onload = shuffle; //Initially the board has to be shuffled once
